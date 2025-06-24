11b2338d145e97ab66e1bb715501e62a7fd5ed66
Malika7188 <asmanodeny@gmail.com>
2025-06-25 01:29:10 +0300

    added main barnners

---
+++ movie-tv-discovery-app/src/components/Banner.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import "./Banner.css";
+
+// const Banner = ({ movie }) => {
+//   return (
+//     <div
+//       className="banner"
+//       style={{
+//         backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+//       }}
+//     >
+//       <div className="banner-content">
+//         <h2>{movie.title}</h2>
+//         <p>{movie.overview.slice(0, 150)}...</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Banner;
