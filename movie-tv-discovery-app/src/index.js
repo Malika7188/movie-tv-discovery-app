73f838d438cc36f809a60b822d7eedef985fd936
Malika7188 <asmanodeny@gmail.com>
2025-06-25 01:32:57 +0300

    updated main function for improved ui

--- movie-tv-discovery-app/src/index.js
+++ movie-tv-discovery-app/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { WatchlistProvider } from "./context/WatchlistContext"; // ✅ import the provider
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
